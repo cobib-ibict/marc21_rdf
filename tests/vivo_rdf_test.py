5494f2213eadb3ae7623946eb804e6030e977c02
Drummerzzz <jf.interatividade@gmail.com>
2023-10-08 14:47:47 -0300

    test: update tests

--- tests/vivo_rdf_test.py
+++ tests/vivo_rdf_test.py
@@ -4,40 +4,42 @@ import pytest
 from faker import Faker
 
 from marc21_rdf import RDF
-from marc21_rdf.types import Publisher
+from marc21_rdf.types import Publication, Publisher
 
 fake = Faker()
 
 
 @pytest.fixture
 def rdf() -> RDF:
-    return RDF(content_id=f'n{randint(100, 999)}')
+    return RDF()
 
 
-def test_rdf_build(rdf: RDF):
-    sut = rdf.build(
-        fake.paragraph(nb_sentences=1),
-        fake.name(),
-        fake.city(),
-        fake.postcode(),
-        'pt-BR',
+@pytest.fixture
+def publication() -> Publication:
+    return Publication(
+        id=randint(100, 999),
+        subject=fake.paragraph(nb_sentences=1),
+        url=f'http://pinakes.ccn.com/article/{randint(100, 999)}',
+        publisher=Publisher(
+            id=randint(100, 999),
+            url=f'http://pinakes.ccn.com/person/{randint(100, 999)}',
+            first_name=fake.name().split(' ')[0],
+            last_name=fake.name().split(' ')[1],
+        ),
+        city=fake.city(),
+        issn=randint(100, 999),
+        language='pt-BR',
     )
+
+
+def test_rdf_build(rdf: RDF, publication: Publication):
+    sut = rdf.build(publication)
     assert isinstance(sut, str)
 
 
-def test_rdf_write(rdf: RDF):
+def test_rdf_write(rdf: RDF, publication: Publication):
     try:
-        rdf.build(
-            fake.paragraph(nb_sentences=1),
-            Publisher(
-                url=f'http://pinakes.ccn.com/person/{randint(100, 999)}',
-                first_name=fake.name().split(' ')[0],
-                last_name=fake.name().split(' ')[1],
-            ),
-            fake.city(),
-            randint(100, 999),
-            'pt-BR',
-        )
+        rdf.build(publication)
         rdf.write('tests/test')
     except Exception:
         assert False
